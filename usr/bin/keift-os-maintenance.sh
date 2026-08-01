e0df86a776c5842d05f949d34d6d5f8f4be448bb
Fırat Özden <fir4tozden@gmail.com>
2026-08-02 01:30:10 +0300

    v1.0.0

--- usr/bin/keift-os-maintenance.sh
+++ usr/bin/keift-os-maintenance.sh
@@ -22,21 +22,21 @@ while ! ping -c 1 1.1.1.1 &> /dev/null; do sleep 1; done
 
 send_notification "Keift OS" "Kurulum devam ediyor..." "Kurulumun tamamlanması birkaç dakika sürebilir."
 
-flatpak remote-delete --force fedora || true
-flatpak remote-delete --force fedora-testing || true
+flatpak remote-delete --system --force fedora || true
+flatpak remote-delete --system --force fedora-testing || true
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo || true
+flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo || true
 
-flatpak install -y flathub org.gnome.Calculator || true
-flatpak install -y flathub org.gnome.Calendar || true
-flatpak install -y flathub org.gnome.clocks || true
-flatpak install -y flathub org.gnome.Contacts || true
-flatpak install -y flathub org.gnome.Epiphany || true
-flatpak install -y flathub org.gnome.Loupe || true
-flatpak install -y flathub org.gnome.Maps || true
-flatpak install -y flathub net.nokyan.Resources || true
-flatpak install -y flathub org.gnome.Showtime || true
-flatpak install -y flathub org.gnome.TextEditor || true
-flatpak install -y flathub org.gnome.Weather || true
+flatpak install -y --system flathub org.gnome.Calculator \
+  org.gnome.Calendar \
+  org.gnome.clocks \
+  org.gnome.Contacts \
+  org.gnome.Epiphany \
+  org.gnome.Loupe \
+  org.gnome.Maps \
+  net.nokyan.Resources \
+  org.gnome.Showtime \
+  org.gnome.TextEditor \
+  org.gnome.Weather || true
 
 send_notification "Keift OS" "Kurulum tamamlandı" "Keift OS'unuz hazır."
