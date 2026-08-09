55036a8144319e819aa5d5c432fb9039784874a0
Fırat Özden <fir4tozden@gmail.com>
2026-08-09 14:16:13 +0300

    v1.0.0

--- src/usr/bin/keift-os-maintenance.sh
+++ src/usr/bin/keift-os-maintenance.sh
@@ -16,8 +16,6 @@ execute() {
 
 while [ ! -f /home/*/.config/gnome-initial-setup-done ]; do sleep 1; done
 
-timeout 10 bash -c "while ! ping -c 1 1.1.1.1 &> /dev/null; do sleep 1; done"
-
 sleep 10
 
 state_file="/etc/keift-os-maintenance-sequence"
