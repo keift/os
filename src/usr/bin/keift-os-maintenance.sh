ac64decdab4e0c15a03a823924e4120af71105c6
Fırat Özden <fir4tozden@gmail.com>
2026-08-19 00:35:57 +0300

    v1.0.0

--- src/usr/bin/keift-os-maintenance.sh
+++ src/usr/bin/keift-os-maintenance.sh
@@ -43,19 +43,20 @@ for ((sequence = current_sequence + 1; sequence <= target_sequence; sequence++))
 
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-    flatpak install -y net.nokyan.Resources
-    flatpak install -y org.gnome.Calculator
-    flatpak install -y org.gnome.Calendar
-    flatpak install -y org.gnome.clocks
-    flatpak install -y org.gnome.Contacts
-    flatpak install -y org.gnome.Epiphany
-    flatpak install -y org.gnome.Loupe
-    flatpak install -y org.gnome.Maps
-    flatpak install -y org.gnome.Papers
-    flatpak install -y org.gnome.Showtime
-    flatpak install -y org.gnome.Snapshot
-    flatpak install -y org.gnome.TextEditor
-    flatpak install -y org.gnome.Weather
+    flatpak install -y net.nokyan.Resources # Resources
+    flatpak install -y org.gnome.Calculator # Calculator
+    flatpak install -y org.gnome.Calendar   # Calendar
+    flatpak install -y org.gnome.clocks     # Clocks
+    flatpak install -y org.gnome.Contacts   # Contacts
+    flatpak install -y org.gnome.Decibels   # Audio Player
+    flatpak install -y org.gnome.Epiphany   # Web
+    flatpak install -y org.gnome.Loupe      # Image Viewer
+    flatpak install -y org.gnome.Maps       # Maps
+    flatpak install -y org.gnome.Papers     # Document Viewer
+    flatpak install -y org.gnome.Showtime   # Video Player
+    flatpak install -y org.gnome.Snapshot   # Camera
+    flatpak install -y org.gnome.TextEditor # Text Editor
+    flatpak install -y org.gnome.Weather    # Weather
 
     execute gsettings reset org.gnome.shell app-picker-layout
   elif [ "${sequence}" -eq 1 ]; then
